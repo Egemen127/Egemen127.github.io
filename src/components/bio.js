098546dafb0ce2211d67e9a1150374c2bc226608
Egemen <egemendurmuss@gmail.com>
2022-06-21 16:53:17 -0400

    minor fixes

--- src/components/bio.js
+++ src/components/bio.js
@@ -4,11 +4,11 @@ import image from "../images/IMG_1068.jpg";
 export default function Bio(){
     return (
         <main>
-        <div className="img-container"><img className="img-profile"src={image}/></div>
+        <div className="img-container"><img className="img-profile"src={image} alt="profile-img"/></div>
         <h2>Egemen Durmus</h2>
         <h4>Java Full Stack Developer</h4>
-        <div className="button-container"><a href="https://www.github.com/Egemen127" target="_blank"><div className="button1">Github</div></a>
-        <a href="https://www.linkedin.com/in/egemen-d-b80941180/" target="_blank"><div className="button2">Linkedin</div></a></div>
+        <div className="button-container"><a href="https://www.github.com/Egemen127" target="_blank" rel="noreferrer" ><div className="button1">Github</div></a>
+        <a href="https://www.linkedin.com/in/egemen-d-b80941180/" target="_blank" rel="noreferrer" ><div className="button2">Linkedin</div></a></div>
         <h3>About</h3>
         <p>I am a Master's student at Pace University in NYC, currently studying software development and engineering. I learned about software engineering theory through my classes. I also learned about fullstack development using java in Genspark training. </p>
         <h3>Interests</h3>
--- src/components/projects.js
+++ src/components/projects.js
@@ -16,27 +16,27 @@ import img12 from "../images/GuessTheNumberCode.PNG";
 export default function MyProjects(){
     return (<div className="project-container">
         <div className="project">
-            <img className="img-project"src={img1}/><img className="img-project" src={img2}/>
+            <img className="img-project"src={img1} alt="project-img"/><img className="img-project" src={img2} alt="project-img"/>
             <h2>Boarding Pass</h2>
         <p>An application that generates tickets according to given input. The generated tickets are stored in a text file. The GUI was done using JavaFX. </p></div>
         <div className="project">
-            <img className="img-project"src={img3}/><img className="img-project" src={img4}/>
+            <img className="img-project"src={img3} alt="project-img"/><img className="img-project" src={img4} alt="project-img"/>
             <h2>Humans vs Goblins</h2>
         <p>A board game where the player gets chased by goblins. In order to win he has to defeat all the goblins and collect the treasures! The GUI was done using JavaFX.</p></div>
         <div className="project">
-            <img className="img-project"src={img5}/><img className="img-project" src={img6}/>
+            <img className="img-project"src={img5} alt="project-img"/><img className="img-project" src={img6} alt="project-img"/>
             <h2>Hangman</h2>
         <p>Classic hangman game written in Java that you can play on command prompt. When the game starts, a unique word is pulled up from a text file that contains more than 50000 words. When the game ends, player's score is stored in a text file.</p></div>
         <div className="project">
-            <img className="img-project"src={img7}/><img className="img-project" src={img8}/>
+            <img className="img-project"src={img7} alt="project-img"/><img className="img-project" src={img8} alt="project-img"/>
             <h2>HOBO.COM</h2>
         <p>A social platform where people looking for a roommate can connect and leave reviews on each other. Web application is developed using flask framework and SqlAlchemy is used for database connection. </p></div>
         <div className="project">
-            <img className="img-project"src={img9}/><img className="img-project" src={img10}/>
+            <img className="img-project"src={img9} alt="project-img"/><img className="img-project" src={img10} alt="project-img"/>
             <h2>Flight Management API</h2>
         <p>A REST API for managing flight and passenger information. This api performs CRUD operations in a database and handles two tables that have manytomany relation with respect to each other. Spring Boot is used to develop the api. Mysql and JPA are used for the database connection and CRUD operations respectively.</p></div>
         <div className="project">
-            <img className="img-project"src={img12}/><img className="img-project" src={img11}/>
+            <img className="img-project"src={img12} alt="project-img"/><img className="img-project" src={img11} alt="project-img"/>
             <h2>Guess The Number</h2>
         <p>A game where the player guesses a random number from 1 to 20. </p></div>
     </div>)
